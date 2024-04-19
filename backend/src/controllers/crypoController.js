6aeca5eff6a940d45a180dd879989521365a0c8d
Marcio <marcio.hickstein@gmail.com>
2024-04-19 09:33:45 -0300

    1. Adjust HashGenerator and unit tests. 2. Separete routes and controllers to MVC pattern

---
+++ backend/src/controllers/crypoController.js
@@ -0,0 +1,42 @@
+const generateHash = (HashGenerator) => {
+    try {
+        const hash = HashGenerator.generate();
+
+        return {
+            string: HashGenerator.getString(),
+            algorithm: HashGenerator.getAlgorithm(),
+            hash,
+        };
+    } catch (error) {
+
+        return {
+            error: true,
+            message: error.message
+        };
+    }
+}
+
+const cryptoController = {
+    generateMd5Hash: (req, res) => {
+        const md5 = new HashGenerator(req.body.string, 'md5');
+
+        return res.json(generateHash(md5));
+    },
+    generateSha1Hash: (req, res) => {
+        const hash = new HashGenerator(req.body.string, 'sha1');
+
+        return res.json(generateHash(hash));
+    },
+    generateSha256Hash: (req, res) => {
+        const hash = new HashGenerator(req.body.string, 'sha256');
+
+        return res.json(generateHash(hash));
+    },
+    generateSha512Hash: (req, res) => {
+        const hash = new HashGenerator(req.body.string, 'sha512');
+
+        return res.json(generateHash(hash));
+    }
+}
+
+module.exports = cryptoController;
